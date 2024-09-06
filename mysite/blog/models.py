5984fa3af703068b5fa0bec66916d8844bda1c80
Ben Collier <benjamincollier18@icloud.com>
2024-09-06 06:14:39 -0500

    Chapter 3

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
+from taggit.managers import TaggableManager
+
 
 
 class PublishedManager(models.Manager):
@@ -35,9 +37,9 @@ class Post(models.Model):
         choices=Status,
         default=Status.DRAFT
     )
-
     objects = models.Manager()  # The default manager.
     published = PublishedManager()  # Our custom manager.
+    tags = TaggableManager()
 
     class Meta:
         ordering = ['-publish']
--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,13 +1,22 @@
 from django.urls import path
-
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap
 from . import views
+from .feeds import LatestPostsFeed
 
 app_name = 'blog'
 
+sitemaps = {
+ 'posts': PostSitemap,
+}
+
 urlpatterns = [
     # Post views
-    # path('', views.post_list, name='post_list'),
-    path('', views.PostListView.as_view(), name='post_list'),
+    path('', views.post_list, name='post_list'),
+    # path('', views.PostListView.as_view(), name='post_list'),
+    path(
+        'tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'
+    ),
     path(
         '<int:year>/<int:month>/<int:day>/<slug:post>/',
         views.post_detail,
@@ -17,4 +26,12 @@ urlpatterns = [
     path(
         '<int:post_id>/comment/', views.post_comment, name='post_comment'
     ),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
+    path('search/', views.post_search, name='post_search'),
+    path(
+        'sitemap.xml',
+        sitemap,
+        {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap'
+    )
 ]
