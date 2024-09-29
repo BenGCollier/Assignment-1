d8393bcda13b9c5e0b707260e228056afd342b87
Ben Collier <benjamincollier18@icloud.com>
2024-09-29 03:56:59 -0500

    Recipe and Post Separation

--- blog_plus/blog/templatetags/blog_tags.py
+++ blog_plus/blog/templatetags/blog_tags.py
@@ -3,7 +3,7 @@ from django import template
 from django.db.models import Count
 from django.utils.safestring import mark_safe
 
-from ..models import Post
+from ..models import Post, Recipe
 
 register = template.Library()
 
@@ -24,6 +24,23 @@ def get_most_commented_posts(count=5):
     return Post.published.annotate(
         total_comments=Count('comments')
     ).order_by('-total_comments')[:count]
+
+@register.simple_tag
+def total_recipes():
+    return Recipe.published.count()
+
+
+@register.inclusion_tag('blog/recipe/latest_recipes.html')
+def show_latest_recipes(count=5):
+    latest_recipes = Recipe.published.order_by('-publish')[:count]
+    return {'latest_recipes': latest_recipes}
+
+
+@register.simple_tag
+def get_most_commented_recipes(count=5):
+    return Recipe.published.annotate(
+        total_recipe_comments=Count('recipe_comments')
+    ).order_by('-total_recipe_comments')[:count]
 
 
 @register.filter(name='markdown')
